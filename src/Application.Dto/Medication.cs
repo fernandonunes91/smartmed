f13f2a0fad715137d79e354d4cbd0bca18749421
Fernando Nunes <fernando.nunes@ebankit.com>
2019-11-16 17:09:17 +0000

    Create presentation and add get all method

---
+++ src/Application.Dto/Medication.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Application.Dto
+{
+    using System;
+
+    public class Medication
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+    }
+}
---
+++ src/Presentation.Api/Controllers/V1/MedicationController.cs
@@ -0,0 +1,23 @@
+﻿namespace Presentation.Api.Controllers.V1
+{
+    using Application.Dto;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [ApiController]
+    [Route("v1/Medication")]
+    public class MedicationController : ControllerBase
+    {
+        /// <summary>
+        /// Get all medications
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Medication>), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            return this.Ok(Enumerable.Empty<Medication>());
+        }
+    }
+}
